0fc98981855c7cb9cd20429227d76b9af6af7bf1
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-05-18 17:40:33 +0400

    added home work

---
+++ SatishApp/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SatishApp
+{
+    public delegate void Convert(double azn);
+    class Order
+    {
+        public double Totalprice;
+        public void Sale(Product product, int count)
+        {
+
+            double a = product.Price * count;
+            Totalprice += a;
+            Console.WriteLine($"TOTAL- { Math.Round(Totalprice, 2)} AZN");
+        }
+
+        public void Discount()
+        {
+            Predicate<double> nodiscount = Totalprice => Totalprice <= 100;
+            Predicate<double> discount10 = Totalprice => Totalprice > 100 && Totalprice < 200;
+            Predicate<double> discount20 = Totalprice => Totalprice >= 200;
+            if (nodiscount(Totalprice))
+            {
+                Console.WriteLine($"Not have discount -  {Math.Round(Totalprice, 2)}");
+            }
+            if (discount10(Totalprice))
+            {
+                Totalprice = Totalprice - Totalprice * 0.1;
+                Console.WriteLine($" 10 % discount -  {Math.Round(Totalprice, 2)}");
+            }
+            if (discount20(Totalprice))
+            {
+                Totalprice = Totalprice - Totalprice * 0.2;
+                Console.WriteLine($" 20 % discount -  {Math.Round(Totalprice, 2)}");
+            }
+
+        }
+    }
+}
---
+++ SatishApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace SatishApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product Tie = new Product("Tie", 30.5, 300);
+            Product Shoes = new Product("Shoes", 199.0, 500);
+            Product Pant = new Product("Pant", 250.99, 500);
+            Product Shirt = new Product("Shirt", 150.5, 1000);
+            Console.WriteLine("\nDamat Tween Menu");
+            Order or = new Order();
+            int selection;
+            int selection2;
+            do
+            {
+                Console.WriteLine("\nPlease choose selection");
+                Console.WriteLine("1 - Tie");
+                Console.WriteLine("2 - Shoes");
+                Console.WriteLine("3 - Pant");
+                Console.WriteLine("4- Shirt");
+                Console.WriteLine("0 - Shoping end");
+                selection = int.Parse(Console.ReadLine());
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine("Please enter the number of products");
+                        selection2 = int.Parse(Console.ReadLine());
+                        or.Sale(Tie, selection2);
+                        break;
+                    case 2:
+                        Console.WriteLine("Please enter the number of products");
+                        selection2 = int.Parse(Console.ReadLine());
+                        or.Sale(Shoes, selection2);
+                        break;
+                    case 3:
+                        Console.WriteLine("Please enter the number of products");
+                        selection2 = int.Parse(Console.ReadLine());
+                        or.Sale(Pant, selection2);
+                        break;
+                    case 4:
+                        Console.WriteLine("Please enter the number of products");
+                        selection2 = int.Parse(Console.ReadLine());
+                        or.Sale(Shirt, selection2);
+                        break;
+                    case 0:
+                        or.Discount();
+                        break;
+
+
+                    default:
+                        break;
+                }
+
+
+            } while (selection!=0);
+        }
+    
+    }
+}
